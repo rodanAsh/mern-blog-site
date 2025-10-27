31afecb9cfe272575cac4e571bdb1d20dd1da765
rodanAsh <aakashash2002@gmail.com>
2025-10-27 18:16:51 +0530

    feat; integrated genai to genrate blog desc with ai

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -2,6 +2,7 @@ import imagekit from "../configs/imageKit.js";
 import Blog from "../models/Blog.js";
 import fs from 'fs'
 import Comment from "../models/Comment.js";
+import main from "../configs/gemini.js";
 
 // add new blog
 export const addBlog = async (req, res) => {
@@ -118,6 +119,22 @@ export const getBlogComments = async (req, res) => {
         res.json({ success: true, comments });
     } catch (error) {
         res.json({ success: false, message: error.message });
+    }
+}
+
+export const generateContent = async (req, res) => {
+    try {
+
+        const { prompt } = req.body;
+
+        const content = await main(prompt + ' Generate a blog content for this topic in simple text format');
+
+        res.json({ success: true, content })
+
+    } catch (error) {
+
+        res.json({ success: false, message: error.message });
+        
     }
 }
 
